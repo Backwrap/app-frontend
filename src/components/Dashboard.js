b271736046b05d21841a779979151e88158fbe09
David Atanda <ololadedavid15@gmail.com>
2022-10-17 12:07:01 +0100

    dashboard izz going

---
+++ src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Dahsboard izz going. Welcome to BackWrap!</h1>
+        </div>
+    );
+};
+
+export default Dashboard;
